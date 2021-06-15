2ae2329e97053e0f592c60789d733d1339c47d0f
unknown <arhersleep@gmail.com>
2021-06-15 16:47:47 +0300

    Нежелательные setup и teardown методы

--- 4.3/test_product_page.py
+++
@@ -1,9 +0,0 @@
-from .pages.product_page import ProductPage
-
-
-def test_guest_can_add_to_card(driver):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    product_page = ProductPage(driver, link)
-    product_page.open()
-    product_page.shold_be_add_button()
-    product_page.add_to_card()
